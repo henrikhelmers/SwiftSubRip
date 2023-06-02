1e411a721de807c3a888149afdd1f46f83029a5f
Henrik Helmers <helmers@me.com>
2023-06-02 09:53:44 +0200

    Initial commit with regex builder

--- Sources/swift-subrip/swift_subrip.swift
+++ Sources/swift-subrip/swift_subrip.swift
@@ -1,6 +1,100 @@
-public struct swift_subrip {
-    public private(set) var text = "Hello, World!"
+import AVKit
+import RegexBuilder
 
-    public init() {
+public struct Subtitle {
+    let from: Double
+    let to: Double
+    let text: String
+    var interval: ClosedRange<Double> {
+        from...to
+    }
+}
+
+public struct Subtitles {
+    var entries: [Subtitle]
+
+    /// File expected to have UTF-8 encoding
+    init(from url: URL) throws {
+        let string = try String(contentsOf: url)
+        entries = try parseSubRip(string)
+    }
+
+    func getSubtitle(at timeStamp: TimeInterval) -> Subtitle? {
+        entries.first(where: {
+            $0.interval.contains(timeStamp)
+        })
+    }
+}
+
+private func parseSubRip(_ payload: String) throws -> [Subtitle] {
+    // Recognize the SubRip format
+
+    let regex = try! Regex(#"([0-9]+)\n([[0-9]:,.]+)\s+-{2}\>\s+([[0-9]:,.]+)\n([\s\S]*?(?=\n{2,}|$))"#)
+
+
+    // Split entry into named groups
+    let index = Reference(Substring.self)
+    let from = Reference(Substring.self)
+    let to = Reference(Substring.self)
+    let text = Reference(Substring.self)
+
+    // 1
+    // 00:00:01,240 --> 00:00:05,080
+    // Dette her er TIDENES MOMENTO!!!
+    let entryRegex = Regex {
+        Capture(as: index) {
+            OneOrMore(.digit)
+        }
+        OneOrMore(.whitespace)
+
+        Capture(as: from) {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf(":,."),
+                    .digit
+                )
+            }
+        }
+        OneOrMore(.whitespace)
+        "-->"
+        OneOrMore(.whitespace)
+        Capture(as: to) {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf(":,."),
+                    .digit
+                )
+            }
+        }
+        OneOrMore(.whitespace)
+
+        Capture(as: text) {
+            OneOrMore(.anyGraphemeCluster)
+        }
+    }
+
+    return payload.ranges(of: regex).compactMap {
+
+        guard let entry = try? entryRegex.wholeMatch(in: payload[$0]) else {
+            fatalError("Beep boop")
+        }
+
+        return Subtitle(from: String(entry[from]).timeInterval,
+                        to: String(entry[to]).timeInterval,
+                        text: String(entry[text]))
+    }
+}
+
+
+private extension String {
+    var timeInterval: TimeInterval {
+        let commaToPeriod = replacingOccurrences(of: ",", with: ".")
+        let components = commaToPeriod.components(separatedBy: [":"])
+        var interval: Double = 0
+
+        for (index, part) in components.reversed().enumerated() {
+            interval += (Double(part) ?? 0) * pow(Double(60), Double(index))
+        }
+        return interval
     }
 }
--- Tests/swift-subripTests/swift_subripTests.swift
+++ Tests/swift-subripTests/swift_subripTests.swift
@@ -2,10 +2,13 @@ import XCTest
 @testable import swift_subrip
 
 final class swift_subripTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(swift_subrip().text, "Hello, World!")
+    func testParser() throws {
+        let url = Bundle.module.url(forResource: "sample1", withExtension: "srt")!
+
+        let subtitles = try! Subtitles(from: url)
+
+        print(subtitles)
+
+        // XCTAssertEqual(swift_subrip().text, "Hello, World!")
     }
 }
